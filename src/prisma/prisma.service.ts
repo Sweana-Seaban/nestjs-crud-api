8a8bce9abf0b93075a439a0802496b799a695291
sweana <sweana@bititude.com>
2024-02-19 15:15:02 +0530

    Add config module for environment variables

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'mysql://root:password@localhost:3306/bookmarks',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
+    console.log(config.get('DATABASE_URL'));
   }
 }
