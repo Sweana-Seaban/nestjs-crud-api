4b0596263755bde995feccaee4070e01c84cdfa2
sweana <sweana@bititude.com>
2024-02-19 15:52:46 +0530

    Add passportJS and jwt module for user authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private configService: ConfigService,
+  ) {}
   async signup(dto: AuthDto) {
     //generate password hash
     const hash = await argon.hash(dto.password);
@@ -17,7 +23,7 @@ export class AuthService {
       });
       delete user.hash;
       //return saved user
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       //isolating error that came from prisma
       if (error instanceof PrismaClientKnownRequestError) {
@@ -43,7 +49,12 @@ export class AuthService {
     //if password incorrect throw exception
     if (!pwMatches) throw new ForbiddenException('Credentials Taken');
     //send back the user
-    delete user.hash;
-    return user;
+    return this.signToken(user.id, user.email);
+  }
+
+  signToken(userId: number, email: string): Promise<string> {
+    const payload = { sub: userId, email };
+    const secret = this.configService.get('JWT_SECRET');
+    return this.jwt.signAsync(payload, { expiresIn: '15m', secret: secret });
   }
 }
