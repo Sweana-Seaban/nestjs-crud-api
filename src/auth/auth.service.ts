d0219852d4495d3ae3028e5d1d0cc97023924a4f
sweana <sweana@bititude.com>
2024-02-19 13:13:51 +0530

    Isolate prisma error in signup logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -10,12 +11,22 @@ export class AuthService {
     //generate password hash
     const hash = await argon.hash(dto.password);
     //save new user in db
-    const user = await this.prisma.user.create({
-      data: { email: dto.email, hash },
-    });
-    delete user.hash;
-    //return saved user
-    return user;
+    try {
+      const user = await this.prisma.user.create({
+        data: { email: dto.email, hash },
+      });
+      delete user.hash;
+      //return saved user
+      return user;
+    } catch (error) {
+      //isolating error that came from prisma
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials Taken');
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
